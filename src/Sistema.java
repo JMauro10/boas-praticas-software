041c8638c12a724b5a1c82132c35c1ec84e944cc
Jose Mauro <josemaurosilva600@gmail.com>
2026-09-08 22:17:31 -0300

    Refatorando o código: melhora da nomenclatura e aplica modularização com a criação do metodo de calcular a media

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,18 +1,25 @@
 public class Sistema {
     public static void main(String[] args) {
-        String n = "Carlos";
-        double a = 8;
-        double b = 7;
-        double c = (a+b)/2;
+        String nomeAluno = "Carlos";
+        double primeiraNota = 8;
+        double segundaNota = 7;
 
-        System.out.println("Aluno: " + n);
-        System.out.println("Media: " + c);
 
-        if (c >= 6){
-            System.out.println("Aprovado");
-        }else {
-            System.out.println("Reprovado");
-        }
+//        System.out.println("Aluno: " + nomeAluno);
+//        System.out.println("Media: " + mediaAluno);
+//
+//        if (mediaAluno >= 6){
+//            System.out.println("Aprovado");
+//        }else {
+//            System.out.println("Reprovado");
+//        }
 
     }
+
+    public static double calcularMedia(double primeiraNota, double segundaNota) {
+         Double mediaAluno = (primeiraNota + segundaNota)/2;
+        return mediaAluno;
+    }
+
+
 }
